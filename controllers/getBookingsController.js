828aff4a41794329489a63619bd9b9d5b80213e9
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2023-03-25 03:30:00 +0530

    Parking History API added

---
+++ controllers/getBookingsController.js
@@ -0,0 +1,18 @@
+const connection = require("../config/database");
+
+function getBookingsController(req, res) {
+  const { email } = req.body;
+
+  // Join the bookings table with the parking_lots table on parking_lots
+  const query = `SELECT bookings.booking_id, bookings.transaction_id, bookings.parkinglot_id, bookings.timestamp, parking_lots.price, parking_lots.name, parking_lots.address, parking_lots.state, parking_lots.city FROM bookings INNER JOIN parking_lots ON bookings.parkinglot_id = parking_lots.id WHERE bookings.email = ?`;
+
+  connection.query(query, [email], (err, result) => {
+    if (err) {
+      res.status(500).json({ error: "Something went wrong" + err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+}
+
+module.exports = getBookingsController;
--- routes/bookingRouter.js
+++ routes/bookingRouter.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
+const getBookingsController = require("../controllers/getBookingsController");
 const bookingController = require("../controllers/bookingController");
 
+router.post("/history", getBookingsController);
 router.post("/create", bookingController);
 
 module.exports = router;
